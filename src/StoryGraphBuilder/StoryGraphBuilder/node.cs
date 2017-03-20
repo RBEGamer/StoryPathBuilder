46a458b2cc9f5aff219da0dbd9c065dd8867045c
Marcel Ochsendorf <marcel.ochsendorf@gmail.com>
2017-03-21 00:23:22 +0100

    move globals

--- src/StoryGraphBuilder/StoryGraphBuilder/node.cs
+++ src/StoryGraphBuilder/StoryGraphBuilder/node.cs
@@ -20,6 +20,9 @@ namespace StoryGraphBuilder
         public String headline_text = "---";
         public String message_text = "---";
 
+
+       
+
         protected int con_box_size = 10;
         protected int text_bumbruch_zeichen_line = 30; //new line >30 chars
 
@@ -42,11 +45,28 @@ namespace StoryGraphBuilder
 
         const int text_bumbruch_zeichen_line = 40;
         const int con_box_size = 10;
+
+        Panel con_box_out;
+        Panel seperation_line_message;
+        Label message_text_l;
+        Panel seperation_line_head_1;
+        Label headline = new Label();
+        Panel seperation_line_head_0;
+        Panel con_box_in;
+
         public  text_node()
         {
             render_panel = new Panel();
             headline_text = "-TEXTNODE-";
             message_text = "---";
+
+             con_box_out = new Panel();
+             seperation_line_message = new Panel();
+             message_text_l = new Label();
+             seperation_line_head_1 = new Panel();
+             headline = new Label();
+             seperation_line_head_0 = new Panel();
+             con_box_in = new Panel();
         }
 
         public  void generate_propery_plane()
@@ -61,7 +81,6 @@ namespace StoryGraphBuilder
             render_panel.Size = new Size(size_w, size_h);
             render_panel.BackColor = Color.PaleVioletRed;
             //CREATE BUBBLE INPUT
-            Panel con_box_in = new Panel();
             con_box_in.Size = new Size(con_box_size, con_box_size);
             con_box_in.Location = new Point((int)(size_w / 2f), size_h);
             con_box_in.BackColor = Color.Red;
@@ -71,7 +90,7 @@ namespace StoryGraphBuilder
             //CREATE LINE
             {
                 size_h += 2;
-                Panel seperation_line_head_0 = new Panel();
+
                 seperation_line_head_0.Location = new Point(0, size_h);
                 seperation_line_head_0.Size = new Size(size_w, 2);
                 seperation_line_head_0.BackColor = Color.Black;
@@ -80,7 +99,7 @@ namespace StoryGraphBuilder
             }
 
             //CREATE HEADLINE BOX
-            Label headline = new Label();
+
             headline.Text = headline_text;
             int hl_w = (int)(headline_text.Length * 12);
             int hl_h = (int)((headline.Font.SizeInPoints * 2f) * (1 + headline_text.Length / text_bumbruch_zeichen_line));
@@ -94,7 +113,7 @@ namespace StoryGraphBuilder
             //CREATE LINE
             {
                 size_h += 2;
-                Panel seperation_line_head_1 = new Panel();
+
                 seperation_line_head_1.Location = new Point(0, size_h);
                 seperation_line_head_1.Size = new Size(size_w, 2);
                 seperation_line_head_1.BackColor = Color.Black;
@@ -104,7 +123,7 @@ namespace StoryGraphBuilder
 
 
             //CREATE TEXT BOX
-            Label message_text_l = new Label();
+
             message_text_l.Text = message_text;
             int me_w = (int)(message_text.Length * 12);
             int me_h = (int)((message_text_l.Font.SizeInPoints * 2f) * (1 + message_text.Length / text_bumbruch_zeichen_line));
@@ -120,7 +139,7 @@ namespace StoryGraphBuilder
             //CREATE LINE
             {
                 size_h += 2;
-                Panel seperation_line_message = new Panel();
+
                 seperation_line_message.Location = new Point(0, size_h);
                 seperation_line_message.Size = new Size(size_w, 2);
                 seperation_line_message.BackColor = Color.Black;
@@ -129,7 +148,7 @@ namespace StoryGraphBuilder
             }
 
             //CREATE BUBBLE INPUT
-            Panel con_box_out = new Panel();
+
             con_box_out.Size = new Size(con_box_size, con_box_size);
             con_box_out.Location = new Point((int)(size_w / 2f), size_h);
             con_box_out.BackColor = Color.Green;
@@ -152,11 +171,28 @@ namespace StoryGraphBuilder
 
     public class option_node : node
     {
+
+        Panel con_box_out;
+        Panel seperation_line_message;
+        Panel seperation_line_head_1;
+        Label headline = new Label();
+        Panel seperation_line_head_0;
+        Panel con_box_in = new Panel();
+
+
         public option_node()
         {
             render_panel = new Panel();
             headline_text = "-OPTIONNODE-";
             message_text = "---";
+
+             con_box_out = new Panel();
+             seperation_line_message = new Panel();
+             seperation_line_head_1 = new Panel();
+             headline = new Label();
+             seperation_line_head_0 = new Panel();
+             con_box_in = new Panel();
+
         }
 
         public void generate_propery_plane()
@@ -171,7 +207,7 @@ namespace StoryGraphBuilder
             render_panel.Size = new Size(size_w, size_h);
             render_panel.BackColor = Color.LightGoldenrodYellow;
             //CREATE BUBBLE INPUT
-            Panel con_box_in = new Panel();
+            
             con_box_in.Size = new Size(con_box_size, con_box_size);
             con_box_in.Location = new Point((int)(size_w / 2f), size_h);
             con_box_in.BackColor = Color.Red;
@@ -179,18 +215,18 @@ namespace StoryGraphBuilder
             render_panel.Controls.Add(con_box_in);
             //TODO REGISTER CLICK EVENT
             //CREATE LINE
-            {
+           
                 size_h += 2;
-                Panel seperation_line_head_0 = new Panel();
+
                 seperation_line_head_0.Location = new Point(0, size_h);
                 seperation_line_head_0.Size = new Size(size_w, 2);
                 seperation_line_head_0.BackColor = Color.Black;
                 render_panel.Controls.Add(seperation_line_head_0);
                 size_h += 5;
-            }
+         
 
             //CREATE HEADLINE BOX
-            Label headline = new Label();
+
             headline.Text = headline_text;
             int hl_w = (int)(headline_text.Length * 12);
             int hl_h = (int)((headline.Font.SizeInPoints * 2f) * (1 + headline_text.Length / text_bumbruch_zeichen_line));
@@ -202,28 +238,26 @@ namespace StoryGraphBuilder
             render_panel.Controls.Add(headline);
 
             //CREATE LINE
-            {
+         
                 size_h += 2;
-                Panel seperation_line_head_1 = new Panel();
                 seperation_line_head_1.Location = new Point(0, size_h);
                 seperation_line_head_1.Size = new Size(size_w, 2);
                 seperation_line_head_1.BackColor = Color.Black;
                 render_panel.Controls.Add(seperation_line_head_1);
                 size_h += 5;
-            }
+            
             //CREATE LINE
-            {
+        
                 size_h += 2;
-                Panel seperation_line_message = new Panel();
                 seperation_line_message.Location = new Point(0, size_h);
                 seperation_line_message.Size = new Size(size_w, 2);
                 seperation_line_message.BackColor = Color.Black;
                 render_panel.Controls.Add(seperation_line_message);
                 size_h += 5;
-            }
+            
 
             //CREATE BUBBLE INPUT
-            Panel con_box_out = new Panel();
+
             con_box_out.Size = new Size(con_box_size, con_box_size);
             con_box_out.Location = new Point((int)(size_w / 2f), size_h);
             con_box_out.BackColor = Color.Green;
